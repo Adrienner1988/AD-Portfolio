74fc8f77ebb3792906b95e37c6088b16be11db2d
Your Name <you@example.com>
2024-04-11 02:13:05 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Contact from './Views/Contact/Contact'
+import Home from './Views/Home/Home'
+import Projects from './Views/Projects/Projects'
+
+function App() {
+
+  return (
+    <>
+     <Home />
+     <Projects />
+     <Contact />
+    </>
+  )
+}
+
+export default App
