90eb44305aa345efef295c187805cda41a26a423
Your Name <you@example.com>
2024-04-11 02:13:05 -0400

    first commit

---
+++ src/Views/Contact/Contact.tsx
@@ -0,0 +1,13 @@
+
+
+const Contact = () => {
+  return (
+    <>
+    <div className="bg-blue-600 text-white p-4">Contact for: job opportunities, collaboration inquiries, or general feedback.
+You can include a contact form, your email address, social media links, or other contact information on a dedicated contact page or in the footer of every page.
+    </div>
+    </>
+  )
+}
+
+export default Contact
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/Views/Home/Home.tsx',
+    './src/Views/Projects/Projects.tsx',
+    "./src/Views/Contact/Contact.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
