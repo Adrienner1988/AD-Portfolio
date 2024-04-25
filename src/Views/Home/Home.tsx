fe10da9c457c6dd810afb74581c97d4a4326acf8
Your Name <you@example.com>
2024-04-25 12:08:47 -0400

    Added in about me and react icons

--- src/Views/Home/Home.tsx
+++ src/Views/Home/Home.tsx
@@ -3,30 +3,32 @@
 const Home = () => {
   return (
     <>
-      <div className="bg-purple text-offWhite p-4">
-        <h1 className="text-charcoal">Software Engineer</h1>
-        <h2 className="">
-          Welcome, I'm <strong className="text-gold">Adrienne Daniels</strong>! thank you
-          for stopping by my little corner of the internet to learn more about me and check out what I have been building.
-        </h2>
+    <div  className="">
+      <h1>Software Engineer</h1>
+      <h2 className="">
+        Welcome, I'm <strong className="text-gold">Adrienne Daniels</strong>! thank you
+        for stopping by my little corner of the internet to learn more about me and check out what I have been building.
+      </h2>
 
-        <h1 className="text-charcoal">&#12296;About Adrienne&#12297;</h1>
-        <p>&#12296;Words about AD&#12297;</p>
+      <h1>&#12296;About Adrienne&#12297;</h1>
+        <p>A versatile full-stack Developer with a knack for crafting innovative projects and solving complex challenges. Beyond my coding prowess, I often explore new destinations or perfect my dance moves on roller skates. With a background that spans various roles and industries, I bring a unique blend of analytical precision and customer-centric thinking to every project. I thrive on collaboration and am eager to contribute my skills to front-end or full-stack engineering teams, bringing energy and expertise to drive impactful results in web development.</p>
 
-        <h1 className="text-charcoal">Skills</h1>
-        <div>
-          <div>HTML Icon</div>
-          <div>CSS Icon</div>
-          <div>JS Icon</div>
-          <div>TS Icon</div>
-          <div>SQL Icon</div>
-          <div>Py Icon</div>
-          <div>React Icon</div>
-          <div>Node.js Icon</div>
-          <div>Tailwind Icon</div>
-          <div>Bootstrap Icon</div>
-        </div>
+        <h1>&#12296;Skills&#12297;</h1>
+      <div>
+        <div className="text-charcoal">HTML Icon</div>
+        <div>CSS Icon</div>
+        <div>JS Icon</div>
+        <div>TS Icon</div>
+        <div>SQL Icon</div>
+        <div>Py Icon</div>
+        <div>React Icon</div>
+        <div>Node.js Icon</div>
+        <div>Tailwind Icon</div>
+        <div>Bootstrap Icon</div>
       </div>
+
+
+    </div >
     </>
   )
 }
