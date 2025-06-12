0f8850d264f35adc1ccae02cebf089adaa2b3c13
adrienner1988 <adriennerdaniels@gmail.com>
2025-06-12 17:29:26 -0400

    Worked on submit button on contact page.

--- src/Views/Contact/Contact.tsx
+++ src/Views/Contact/Contact.tsx
@@ -45,9 +45,9 @@ const Contact = () => {
       className="px-6 py-16 md:px-20">
 
       <div className="mx-auto max-w-3xl">
-        <h1 className="mb-6 mt-16 text-white text-center text-4xl font-bold">
+        <h2 className="mb-6 mt-16 text-white text-center text-4xl font-bold">
           Get in Touch
-        </h1>
+        </h2>
 
         <form
           ref={formRef}
@@ -70,10 +70,11 @@ const Contact = () => {
           />
           <button
             type="submit"
-            className="mt-2 w-full rounded-md bg-purple-600 px-4 py-2 font-semibold text-white transition  hover:bg-purple-700"
+            className="mt-6 w-full rounded-xl bg-gradient-to-r from-purple-500 via-pink-500 to-fuchsia-500 px-6 py-3 text-lg font-semibold text-white shadow-md transition-all duration-300 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-pink-400"
           >
             Send Message
           </button>
+
           {status && <p className="text-center text-sm">{status}</p>}
         </form>
 
